f187fbc6a3332198e11093bef2255fee145373eb
Hongtian Zang <zangj4548@wrdsb.ca>
2023-01-03 15:34:20 -0500

    navbar collapse

--- src/components/navbar/NavBar.vue
+++ src/components/navbar/NavBar.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="sticky top-0 h-screen w-60 min-w-[60px] bg-gray-500 text-white p-2">
-    <div class="justify-center items-center m-2">
-      <div class="w-10 h-10 rounded-full justify-center items-center
-			hover:bg-green-400 active:bg-green-500 cursor-pointer transition-all">
+  <div class="sticky top-0 h-screen bg-gray-500 text-white p-2">
+    <div class="justify-center items-center p-1">
+      <button @click="collapse" :class="`flex justify-center content-center transition-all hover:bg-green-400 p-3 rounded-full`">
         <font-awesome-icon icon="fa-solid fa-bars"/>
-      </div>
+      </button>
     </div>
-    <NavButton icon="home" to="/">Home</NavButton>
-    <NavButton icon="gears" to="/generate">Generator</NavButton>
+    <div :class="`${open ? 'hidden' : 'block'}`">
+      <NavButton icon="home" to="/">Home</NavButton>
+      <NavButton icon="gears" to="/generate">Generator</NavButton>
+    </div>
   </div>
 </template>
 
@@ -18,7 +19,16 @@ import NavButton from "./NavButton.vue";
 export default {
   components: {
     NavButton,
+  },
+  data() {
+    return {
+      open: false
+    }
+  },
+  methods: {
+    collapse() {
+      this.open = !this.open;
+    },
   }
 }
-
 </script>
--- src/components/navbar/NavButton.vue
+++ src/components/navbar/NavButton.vue
@@ -1,7 +1,7 @@
 <template>
 	<router-link :to="to">
 		<div
-			class="rounded-md hover:bg-green-400 active:bg-green-500 p-1 m-1 w-aut cursor-pointer transition-all text-white">
+			class="rounded-md hover:bg-green-400 active:bg-green-500 flex items-center p-1 m-1 cursor-pointer transition-all text-white">
 			<font-awesome-icon :icon="['fas', icon]" class="mx-2" />
 			<slot>REPLACEME</slot>
 		</div>
